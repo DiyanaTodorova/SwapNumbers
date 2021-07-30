2d4c93c5c8fc5ec019babdfcf0ccfad4428145ae
DiyanaTodorova <diyanasttodorova@gmail.com>
2021-07-30 13:22:07 +0300

    Add project files.

---
+++ SwapNumbers/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace SwapNumbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double a = double.Parse(Console.ReadLine());
+            double b = double.Parse(Console.ReadLine());
+            (a, b) = (b, a);
+            Console.WriteLine("a: "+ a);
+            Console.WriteLine("b: "+ b);
+        }
+    }
+}
